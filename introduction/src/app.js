88d822076a6ff30749452e64bdcf9da2c1b4eeb8
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-20 12:49:55 +0330

    create introduction directory and learn how tsconfig works

---
+++ introduction/src/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let test = "hi";
+test = 1;
+//# sourceMappingURL=app.js.map
---
+++ introduction/src/app.ts
@@ -0,0 +1,2 @@
+let test = "hi";
+test = 1;
