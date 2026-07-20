5e5dfbf54054d983696f94a12d2f3b401c6a9a9e
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-20 13:25:06 +0330

    built in types

---
+++ S1_basic_types/src/types.ts
@@ -0,0 +1,11 @@
+//JS => number,string,null,undefined,object,boolean
+//TS => ...JS,any,never,unknown,enum,tuple
+
+//Built in types:
+let price: number = 100_000;
+let name: string = "mahdiyar";
+let isMarried: boolean = true;
+const email = "test@gmail.com";
+
+//Any type:
+let value
