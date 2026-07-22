ce4d2d05e7939c9fe046e92e7a4e72a087d32d1f
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-22 10:52:07 +0330

    learn built in generics

--- S4_generics/1.generics.ts
+++ S4_generics/1.generics.ts
@@ -1,4 +1,4 @@
-//Generics: used for functions , classes, interfaces ,.. ,
+//Generics: used for functions , classes, interfaces ,.. , built-in generics
 //generics used for  re-useable component , function , code , class
 
 //in below scenario we should create two different func to handle number and string in input
@@ -26,3 +26,25 @@ const convertedNumbers1 = getArray<number>([1, 2, 3]).map((item) =>
 const convertedStrings1 = getArray<string>(["a", "b"]).map((item) =>
   item.toUpperCase(),
 );
+
+//built in generics: (the generics which is already we have in js and there is no difference between them in js and ts)
+//#sample 1:
+const tsArray: string[] = [];
+const jsArray: Array<string> = [];
+
+//#sample 2:
+const tsPromise = new Promise<string>((res, rej) => {
+  setTimeout(() => {
+    res("resolved");
+  }, 1000);
+});
+tsPromise.then((data) => console.log(data.toUpperCase()));
+
+
+const jsPromise: Promise<string> = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("resolved");
+  }, 1000);
+});
+jsPromise.then((data) => console.log(data.toUpperCase()));
+
