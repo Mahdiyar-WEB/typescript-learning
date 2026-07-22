8abd155ecfd61e7424e691cce93b0797e4eae91f
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-07-22 11:54:47 +0330

    learn pick and omit generics utility in ts

--- S4_generics/3.predefined_generics.ts
+++ S4_generics/3.predefined_generics.ts
@@ -6,6 +6,8 @@ interface Person {
 }
 
 interface PartialPerson extends Partial<Person> {}
+//or
+type PartialPerson1 = Partial<Person>;
 
 const user: PartialPerson = {};
 const user2: Person = {};
@@ -18,6 +20,25 @@ interface OptionalPerson {
 }
 
 interface RequiredPerson extends Required<OptionalPerson> {}
+//or
+type RequiredPerson1 = Required<OptionalPerson>;
 
 const user3: OptionalPerson = {};
 const user4: RequiredPerson = {};
+
+//3.omit: (remove some properties from another interface or type and create new type or interface)
+interface PersonWithoutEmailAndName extends Omit<Person, "email" | "name"> {}
+//or
+type PersonWithoutEmailAndName1 = Omit<Person, "email" | "name">;
+
+const personWithoutEmail: PersonWithoutEmailAndName = { age: 23 };
+
+//4.pick (pick some properties from another interface or type and create new type or interface)
+interface PersonWithEmailAndName extends Pick<Person, "email" | "name"> {}
+//or
+type PersonWithEmailAndName1 = Pick<Person, "email" | "name">;
+
+const personWithEmailAndName: PersonWithEmailAndName = { email: "", name: "" };
+
+
+
